8933124b0b58464b4009c02bb7f60d5db145678f
Syarifuddin Sulthon <sulthon.syrif07@gmail.com>
2021-12-16 14:21:51 +0700

    change style card pokemon

--- components/Card.js
+++ components/Card.js
@@ -31,12 +31,14 @@ const Card = ({ item }) => {
       className="card cursor-pointer relative w-full text-white rounded-lg p-3 overflow-hidden"
       style={{backgroundColor: bgColorType }}
       onClick={() => goToDetail(item.name)}>
-      <div className="absolute top-0 right-0 p-3 text-sm font-bold italic">#{addPad(number)}</div>
       <div>
-        <div className="font-bold pb-3 text-lg capitalize">{item.name}</div>
+        <div className='flex flex-col-reverse sm:flex-row justify-between'>
+          <div className="font-bold pb-5 text-lg capitalize leading-6">{item.name}</div>
+          <div className="text-xs font-medium italic opacity-60">#{addPad(number)}</div>
+        </div>
         {types.map((type, index) => (
           <span
-            className="table text-sm mb-2 rounded-full py-1 px-4 bg-white bg-opacity-30 capitalize"
+            className="table text-xs sm:text-sm mb-2 rounded-full py-1 px-2 sm:px-4 bg-white bg-opacity-30 capitalize"
             key={index}>{type.type.name}
           </span>
         ))}
@@ -44,11 +46,13 @@ const Card = ({ item }) => {
 
       <img src="/pokeball.png" className="w-24 absolute -bottom-4 -right-4 opacity-30" alt="bg-pokeball" />
       <div className="img-container">
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`}
-          className="absolute -bottom-1 -right-2"
-          alt="pokemon-image"
-        />
+        {number && (
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`}
+            className="absolute -bottom-1 -right-1"
+            alt="pokemon-image"
+          />
+        )}
       </div>
     </div>
   )
