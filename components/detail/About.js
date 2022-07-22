92fff11709ab03d0ab3da3efb575c0fbf093907b
Syarifuddin Sulthon <syarifuddin@mamiteam.com>
2022-07-23 03:01:50 +0700

    change color sub-title at about component

--- components/detail/About.js
+++ components/detail/About.js
@@ -7,15 +7,17 @@ const About = ({ height, weight, baseExperience, types }) => {
         <h1 className='font-bold text-lg pb-3'>About</h1>
         <div className='py-4 grid grid-cols-3 gap-3 about-wrapper'>
           <div>
-            <div className='text-sm font-medium pb-1'>Height</div>
+            <div className='text-sm font-medium pb-1 text-gray-400'>Height</div>
             <div className='font-bold'>{height / 10} m</div>
           </div>
           <div>
-            <div className='text-sm font-medium pb-1'>Weight</div>
+            <div className='text-sm font-medium pb-1 text-gray-400'>Weight</div>
             <div className='font-bold'>{weight / 10} kg</div>
           </div>
           <div>
-            <div className='text-sm font-medium pb-1'>Base EXP</div>
+            <div className='text-sm font-medium pb-1 text-gray-400'>
+              Base EXP
+            </div>
             <div className='font-bold'>{baseExperience}</div>
           </div>
           <div className='pt-8'>
